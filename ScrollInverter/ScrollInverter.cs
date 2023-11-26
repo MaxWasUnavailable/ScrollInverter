42bafd7d6851950671f21a23feda1549872f68d0
Max <89798523+MaxWasUnavailable@users.noreply.github.com>
2023-11-26 17:16:34 +0100

    Initial commit

---
+++ ScrollInverter/ScrollInverter.cs
@@ -0,0 +1,19 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace ScrollInverter;
+
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+public class ScrollInverter : BaseUnityPlugin
+{
+    internal new static ManualLogSource Logger;
+    public static ScrollInverter Instance { get; private set; }
+    private Harmony _harmony;
+    private bool _isPatched;
+    private void Awake()
+    {
+        // Plugin startup logic
+        Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+    }
+}
